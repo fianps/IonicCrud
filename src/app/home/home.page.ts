86b8263aa3295a73988158dbb6ccab142261929b
fianps <90088063+fianps@users.noreply.github.com>
2022-11-29 13:06:59 +0700

    Final

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,82 @@
+import { Component, ViewChild } from '@angular/core';
+import { StorageService, Item } from '../services/storage.service';
+import { Platform, ToastController, IonList } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  items: Item[] = [];
+
+  newItem: Item = <Item>{};
+
+  @ViewChild('mylist') mylist: IonList;
+
+  constructor(
+    private storageService: StorageService,
+    private plt: Platform,
+    private toastController: ToastController
+  ) {
+    this.plt.ready().then(() => {
+      this.loadItems();
+    });
+  }
+  addItem() {
+    this.newItem.modified = Date.now();
+    this.newItem.id = Date.now();
+
+    this.storageService.addItem(this.newItem).then((item) => {
+      this.newItem = <Item>{};
+      this.showToast('Ruangan Berhasil Ditambah!');
+      this.loadItems(); // Or add it to the array directly
+    });
+  }
+
+  async showToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  updateItem(item: Item) {
+    item.title = this.newItem.title;
+    item.value = this.newItem.value;
+    item.modified = Date.now();
+
+    this.storageService.updateItem(item).then((item) => {
+      this.newItem = <Item>{};
+      this.showToast('Ruangan Berhasil Diubah!');
+      this.mylist.closeSlidingItems();
+      this.loadItems(); // Or add it to the array directly
+    });
+  }
+
+  // updateItem(item: Item) {
+  //   item.title = `UPDATED: ${item.title}`;
+  //   item.modified = Date.now();
+
+  //   this.storageService.updateItem(item).then((item) => {
+  //     this.showToast('Item updated!');
+  //     this.mylist.closeSlidingItems(); // Fix or sliding is stuck afterwards
+  //     this.loadItems(); // Or update it inside the array directly
+  //   });
+  // }
+
+  deleteItem(item: Item) {
+    this.storageService.deleteItem(item.id).then((item) => {
+      this.showToast('Ruangan Berhasil Dihapus!');
+      this.mylist.closeSlidingItems(); // Fix or sliding is stuck afterwards
+      this.loadItems(); // Or splice it from the array directly
+    });
+  }
+
+  loadItems() {
+    this.storageService.getItems().then((items) => {
+      this.items = items;
+    });
+  }
+}
